4d2fd333f09950a7e34dcb6ada3ab4a5212086a8
goto <gogo8bit@yahoo.co.jp>
2022-07-26 00:10:53 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+let circle = document.getElementById("circle");
+let upBtn = document.getElementById("upBtn");
+let downBtn = document.getElementById("downBtn");
+
+
+let rotateValue = circle.style.transform;
+let rotateSum;
+
+upBtn.onclick = function() {
+    /* 時計回り */
+    rotateSum = rotateValue + "rotate(-90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+}
+
+downBtn.onclick = function() {
+    /* 半時計回り */
+    rotateSum = rotateValue + "rotate(90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+}
+
+
